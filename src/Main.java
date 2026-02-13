a8634c21899e94456c732514536debe8501fd845
Nicox363 <nlamorgiabondar@gmail.com>
2026-02-13 10:08:46 +0100

    Implementada función suma

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Calculadora Empresa v1");
+        // Añade estas líneas:
+        System.out.println("Función suma implementada");
+        System.out.println("Función resta implementada");
     }
 }
