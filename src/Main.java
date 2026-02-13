fe0b46fdee6a1914cb52bd995d262b88d5ba5034
Nicox363 <nlamorgiabondar@gmail.com>
2026-02-13 10:18:16 +0100

    Finalizando corrección para producción

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Calculadora Empresa v1");
+        // Añade estas líneas:
         System.out.println("Función suma implementada");
         System.out.println("Función resta implementada");
-        System.out.println(10 / 0); // Este es el error solicitado en la Parte 6
     }
 }
